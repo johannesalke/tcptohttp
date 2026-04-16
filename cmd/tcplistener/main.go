fa3684428955e4cd138967703408e63189de64db
johannesalke <johannesalke@gmail.com>
2026-04-16 22:47:46 +0200

    Reorganized projectstructure. Added UDP sender.

---
+++ cmd/tcplistener/main.go
@@ -0,0 +1,63 @@
+package tcplistener
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"strings"
+)
+
+func main() {
+
+	listener, err := net.Listen("tcp", ":42069")
+	rr("Error setting up TCP listener: ", err)
+	defer listener.Close()
+	for {
+		connection, err := listener.Accept()
+		rr("Error accepting connection: ", err)
+		fmt.Println("Connection accepted!")
+		ch := getLinesChannel(connection)
+		for line := range ch {
+			fmt.Printf("read: %s\n", line)
+		}
+		fmt.Println("Connection closed!")
+
+	}
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	slice := make([]byte, 8)
+	var str string
+	var sections []string
+
+	go func() {
+		for {
+			n, err := f.Read(slice)
+			if n == 0 {
+				ch <- str
+				close(ch)
+				f.Close()
+				return
+			}
+			rr("Error reading from reader: ", err)
+
+			str += string(slice[:n])
+			sections = strings.Split(str, "\n")
+			if len(sections) == 2 {
+				ch <- sections[0]
+
+				str = sections[1]
+			}
+
+		}
+	}()
+	return ch
+}
+
+func rr(message string, err error) {
+	if err != nil {
+		fmt.Print("Error: ", err, "\n")
+	}
+}
---
+++ cmd/udpsender/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp", "localhost:42069")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+	defer conn.Close()
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		line, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+		_, err = conn.Write([]byte(line))
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+	}
+
+}
